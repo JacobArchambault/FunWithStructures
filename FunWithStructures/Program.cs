9e64bde863fb8499b7b20ef50f87c821a1f82cd0
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-19 12:19:57 -0400

    Add Point struct

--- FunWithStructures/Program.cs
+++ FunWithStructures/Program.cs
@@ -10,6 +10,42 @@ namespace FunWithStructures
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** A First Look at Structures *****\n");
+
+            // Create an initial Point.
+            Point myPoint;
+            myPoint.X = 349;
+            myPoint.Y = 76;
+            myPoint.Display();
+
+            // Adjust the X and Y values.
+            myPoint.Increment();
+            myPoint.Display();
+            Console.ReadLine();
+        }
+
+        struct Point
+        {
+            public int X;
+            public int Y;
+
+            // Add 1 to the (X, Y) position.
+            public void Increment()
+            {
+                X++; Y++;
+            }
+
+            // Subtract 1 from the (X, Y) position.
+            public void Decrement()
+            {
+                X--; Y--;
+            }
+
+            // Display the current position.
+            public void Display()
+            {
+                Console.WriteLine("X = {0}, Y = {1}", X, Y);
+            }
         }
     }
 }
